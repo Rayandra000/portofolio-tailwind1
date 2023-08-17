0f2b97c2f76c65aef3f70e61ff192e1144348ed4
RayandraRhezapaleva <rdra6569@gmail.com>
2023-08-17 10:05:36 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./index.html", "./src/input.css"],
+  theme: {
+    fontFamily: {
+      monstserrat: ["Montserrat", "sans-serif"],
+      inter: ["Inter", "sans-serif"],
+      primary: ["Open sans", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        primary: "#0872BF",
+        background: "#141A1A",
+      },
+    },
+  },
+  plugins: [],
+};
